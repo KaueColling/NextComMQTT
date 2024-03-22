6fc7c431bf91717a629de1685eadeaafd20dc78a
rodexiy <rodexcontato@gmail.com>
2024-03-21 21:51:47 -0300

    fazer as cenoureas ainda
    awa

--- src/app/Dashboard/page.tsx
+++ src/app/Dashboard/page.tsx
@@ -4,8 +4,12 @@ import CardEstufa from "@/components/CardEstufa";
 export default function dashboard() {
     return (
         <>
-            <div className="flex flex-row items-center justify-center pt-8 ">
-                <CardEstufa topico="legumes" item="pimentao"></CardEstufa>
+            <div>
+                <div className="flex flex-row items-center justify-center pt-8 gap-8">
+                    <CardEstufa topico="legumes" item="pimentao"></CardEstufa>
+                    {/* <CardEstufa topico="legumes" item="cenoura"></CardEstufa>
+                    <CardEstufa topico="legumes" item="abobora"></CardEstufa> */}
+                </div>
             </div>
         </>
     );
